d919c3ec727cb63fa5121a0d6233f6f68e452753
EmmaZhang <emmazxd@gmail.com>
2024-04-08 16:31:04 +1000

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=256)
+    breed = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
